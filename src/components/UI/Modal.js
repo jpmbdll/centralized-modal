431d55691004fe60ee969697fa545a56fc55772e
Joshua Badillo <jpmbdll@gmail.com>
2022-09-14 16:00:27 +0800

    Added todos

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,6 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { Modal } from "antd";
 import { modalActions } from "../../store/modal-slice";
 
+// TODO
+// - Use Chakra UI modal
+// - Implement dynamic button actions
+// - investigate if using react portal to render modal is possible
+// - allign design with partner console
+
 const TestModal = () => {
   const dispatch = useDispatch();
   const { visible, bodyProps } = useSelector((state) => state.modal);
