2125be65d37420ec314c38800922e802149f8f3e
Joshua Badillo <jpmbdll@gmail.com>
2022-09-14 15:46:43 +0800

    Initial working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { useDispatch } from "react-redux";
 import { modalActions } from "./store/modal-slice";
 import { Button } from "antd";
-import openModal from "./components/UI/Modal";
 
 const Content1 = () => {
   return <h1>Test modal 1</h1>;
@@ -16,11 +15,11 @@ function App() {
   const dispatch = useDispatch();
 
   const showModalOne = () => {
-    dispatch(modalActions.showModal({ bodyProps: <Content1/> }));
+    dispatch(modalActions.showModal({ bodyProps: <Content1 /> }));
   };
 
   const showModalTwo = () => {
-    dispatch(modalActions.showModal({ bodyProps: <Content2/> }));
+    dispatch(modalActions.showModal({ bodyProps: <Content2 /> }));
   };
   return (
     <Fragment>
